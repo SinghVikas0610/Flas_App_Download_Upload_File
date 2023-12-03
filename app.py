2b4a4f531a33e961fc3ea93892344076c3e86f6c
Vikas Singh <singhvikas0610@gmail.com>
2023-12-03 18:00:34 +0530

    Download_Upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from distutils.log import debug
 from fileinput import filename 
 from flask import *  
 app = Flask(__name__)   
-UPLOAD_FOLDER = 'C:/Users/Vikas/Desktop/ReactApp'
+UPLOAD_FOLDER = 'E:/Training/DataScience/Flask/Flask_App/file uploading'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/')   
@@ -14,7 +14,10 @@ def success():
     if request.method == 'POST':   
         f = request.files['file'] 
         f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))
-        return render_template("Acknowledgement.html", name = f.filename)   
-  
+        return render_template("Acknowledgement.html", name = f.filename) 
+@app.route('/download')  
+def download():
+    p="server.jpg"
+    return send_file(p,as_attachment=True)
 if __name__ == '__main__':   
     app.run(debug=True)
