4c6d435ad342989c6f8fd2aa3f3a9c6146869b69
Vikas Singh <singhvikas0610@gmail.com>
2023-12-03 12:45:33 +0530

    User_Session

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, redirect, request, session
+# The Session instance is not used for direct access, 
+# you should always use flask_session
+from flask_session import Session
+from datetime import timedelta
 app = Flask(__name__)
-@app.route('/')
-def home():
-    return "You are in Home Page"
-# Use of <converter: variable name> in the 
-# route() decorator.
-@app.route('/allow/<int:Number>')
-def allow(Number):
-    if Number < 25:
-        return f'Hi! You Are Allowed with {Number}'
-    else:
-       return f'Hello! You are not allowed with {Number}'
-if __name__ == '__main__':
-    app.run()
+app.config["SESSION_PERMANENT"] = True
+app.permanent_session_lifetime = timedelta(minutes=1)
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+@app.route("/")
+def index():
+    if not session.get("name"):
+        return redirect("/login")
+    return render_template('index.html')
+ 
+ 
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session["name"] = request.form.get("name")
+        if session["name"]=="Singh":
+            return redirect("/")
+        else:
+            return render_template("sorry.html")
+    return render_template("login.html")
+ 
+ 
+@app.route("/logout")
+def logout():
+    session["name"] = None
+    return redirect("/")
+ 
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
