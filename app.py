80630421e36c8f5a75c808288ae6b4e0c500e758
Vikas Singh <singhvikas0610@gmail.com>
2023-12-03 17:11:04 +0530

    File Upload

--- app.py
+++ app.py
@@ -1,36 +1,20 @@
-from flask import Flask, render_template, redirect, request, session
-# The Session instance is not used for direct access, 
-# you should always use flask_session
-from flask_session import Session
-from datetime import timedelta
+from distutils.log import debug 
+from fileinput import filename 
+from flask import *  
+app = Flask(__name__)   
+UPLOAD_FOLDER = 'C:/Users/Vikas/Desktop/ReactApp'
 app = Flask(__name__)
-app.config["SESSION_PERMANENT"] = True
-app.permanent_session_lifetime = timedelta(minutes=1)
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-@app.route("/")
-def index():
-    if not session.get("name"):
-        return redirect("/login")
-    return render_template('index.html')
- 
- 
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    if request.method == "POST":
-        session["name"] = request.form.get("name")
-        if session["name"]=="Singh":
-            return redirect("/")
-        else:
-            return render_template("sorry.html")
-    return render_template("login.html")
- 
- 
-@app.route("/logout")
-def logout():
-    session["name"] = None
-    return redirect("/")
- 
- 
-if __name__ == "__main__":
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+@app.route('/')   
+def main():   
+    return render_template("index.html")   
+import os
+@app.route('/success', methods = ['POST'])   
+def success():   
+    if request.method == 'POST':   
+        f = request.files['file'] 
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))
+        return render_template("Acknowledgement.html", name = f.filename)   
+  
+if __name__ == '__main__':   
     app.run(debug=True)
